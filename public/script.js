5d4a4b480b84f78653e41753b9b4c56317ff2760
Glitch (glitch-hello-node) <none>
2025-06-21 03:14:41 +0000

    ⛺️🚌 Checkpoint ./package.json:79248149/1362 ./public/script.js:79248149/25

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ let playlistSongs = [];
 
 async function loadCSVData() {
   try {
-    const response = await fetch('/data');
+    const response = await fetch('');
     if (!response.ok) throw new Error('Erreur HTTP : ' + response.status);
     const data = await response.json();
     csvData = data;
